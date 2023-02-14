3ba7b1d660ffadbaa35aa1fb215fa3a6d7fe6c4b
XSYghostlike <xiashiyang1993@gmail.com>
2023-02-14 02:09:20 -0500

    std test

---
+++ src/Q84/Main.java
@@ -0,0 +1,8 @@
+package Q84;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.print(s.largestRectangleArea(new int[] {1,2,2}));
+    }
+}
